880343c08b4cbcf5088fd72af9cff7f327e6df10
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-02 14:06:30 +0530

    constants & iota

---
+++ 01-intro/03-constants.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		const pi float32 = 3.14
+		fmt.Println(pi)
+	*/
+
+	/*
+		const (
+			pi          float32 = 3.14
+			app_version string  = "1.0.0"
+		)
+	*/
+
+	const (
+		pi          = 3.14
+		app_version = "1.0.0"
+	)
+	fmt.Println(pi)
+}
