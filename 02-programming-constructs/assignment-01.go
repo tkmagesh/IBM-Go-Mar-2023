21e449444561f452f49acf2f4b5fa32ebb4c719f
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-02 15:35:38 +0530

    assignment-01 - solution

--- 02-programming-constructs/assignment-01.go
+++ 02-programming-constructs/assignment-01.go
@@ -23,3 +23,41 @@ if the user inpout is not (1-5)
 	print "Invalid choice"
 	IMPORTANT : display the menu again
 */
+
+package main
+
+import "fmt"
+
+func main() {
+	var userChoice, x, y, result int
+	for {
+		fmt.Println("1. Add")
+		fmt.Println("2. Subtract")
+		fmt.Println("3. Multiply")
+		fmt.Println("4. Divide")
+		fmt.Println("5. Exit")
+		fmt.Println("Enter your choice:")
+		fmt.Scanln(&userChoice)
+
+		if userChoice == 5 {
+			break
+		}
+		if userChoice < 1 || userChoice > 5 {
+			fmt.Println("Invalid Choice")
+			continue
+		}
+		fmt.Println("Enter the value :")
+		fmt.Scanln(&x, &y)
+		switch userChoice {
+		case 1:
+			result = x + y
+		case 2:
+			result = x - y
+		case 3:
+			result = x * y
+		case 4:
+			result = x / y
+		}
+		fmt.Println("Result :", result)
+	}
+}
