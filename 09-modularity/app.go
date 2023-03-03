caf815c33b4798886fca5ef8853f6bbb8914104c
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-03 16:39:21 +0530

    modules & packages - updates

--- 09-modularity/app.go
+++ 09-modularity/app.go
@@ -3,12 +3,15 @@ package main
 import (
 	"fmt"
 
+	"github.com/fatih/color"
+
 	"github.com/tkmagesh77/ibm-go-mar-2023/09-modularity/calculator"
 	"github.com/tkmagesh77/ibm-go-mar-2023/09-modularity/calculator/utils"
 )
 
 func main() {
-	fmt.Println("App executed")
+	// fmt.Println("App executed")
+	color.Red("App executed")
 	fmt.Println(calculator.Add(100, 200))
 	fmt.Println(calculator.Subtract(100, 200))
 	fmt.Println("Operation Count :", calculator.OpCount())
--- 09-modularity/go.mod
+++ 09-modularity/go.mod
@@ -1,3 +1,11 @@
 module github.com/tkmagesh77/ibm-go-mar-2023/09-modularity
 
 go 1.18
+
+require github.com/fatih/color v1.14.1
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/sys v0.5.0 // indirect
+)
