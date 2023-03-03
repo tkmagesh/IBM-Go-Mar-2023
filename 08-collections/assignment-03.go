28720e929030620960fd2db45b762ecab45274e1
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-03 14:17:07 +0530

    assignments 03 & 04 - problem statement

---
+++ 08-collections/assignment-03.go
@@ -0,0 +1,11 @@
+/* Write a function that returns all the prime numbers beween the given start and end */
+
+package main
+
+func main() {
+	getPrimes(3, 100)
+}
+
+func getPrimes(start, end int) {
+	/* generate the prime numbers and return */
+}
