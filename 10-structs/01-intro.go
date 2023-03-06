7b5e4a687e1bfb770d8a84b19bfe4f33ddaa8163
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-06 09:58:28 +0530

    structs intro

---
+++ 10-structs/01-intro.go
@@ -0,0 +1,86 @@
+package main
+
+import "fmt"
+
+type Product struct {
+	Id   int
+	Name string
+	Cost float32
+}
+
+func main() {
+	// var product Product
+	// var product Product = Product{100, "Pen", 10}
+	// var product Product = Product{Id: 100, Name: "Pen", Cost: 10}
+	/*
+		var product Product = Product{
+			Id:   100,
+			Name: "Pen",
+			Cost: 10,
+		}
+	*/
+	/*
+		var product = Product{
+			Id:   100,
+			Name: "Pen",
+			Cost: 10,
+		}
+	*/
+	product := Product{
+		Id:   100,
+		Name: "Pen",
+		Cost: 10,
+	}
+	// fmt.Println(product)
+	// fmt.Printf("%#v\n", product)
+	fmt.Printf("%+v\n", product)
+
+	fmt.Println("Accessing the fields")
+	fmt.Printf("Id = %d, Name = %q, Cost = %0.2f\n", product.Id, product.Name, product.Cost)
+
+	fmt.Println("Updating the fields")
+	product.Cost = 9
+	fmt.Printf("Id = %d, Name = %q, Cost = %0.2f\n", product.Id, product.Name, product.Cost)
+
+	fmt.Println("Structs are values")
+	newProduct := product // assignment will result in a new copy
+	newProduct.Name = "Fountain Pen"
+	fmt.Printf("product : Id = %d, Name = %q, Cost = %0.2f\n", product.Id, product.Name, product.Cost)
+	fmt.Printf("newProduct : Id = %d, Name = %q, Cost = %0.2f\n", newProduct.Id, newProduct.Name, newProduct.Cost)
+
+	fmt.Println("Equality")
+	p2 := Product{Id: 100, Name: "Pen", Cost: 9}
+	fmt.Println("product : ", product)
+	fmt.Println("p2 	 : ", p2)
+	fmt.Println("product == p2 :", product == p2) // struct instances are compared by value
+
+	fmt.Println("Pointers")
+	/*
+		pencilValue := Product{Id: 200, Name: "Pencil", Cost: 5}
+		pencil := &pencilValue
+	*/
+	pencil := &Product{Id: 200, Name: "Pencil", Cost: 5}
+	fmt.Println((*pencil).Id)
+	fmt.Println(pencil.Id) // fields can be accessed WITHOUT dereferencing the pointer of a struct
+	fmt.Printf("%p\n", pencil)
+
+	/*
+		pencilValue := Product{Id: 200, Name: "Pencil", Cost: 5}
+		pencilPtr1 := &pencilValue
+		pencilPtr2 := &pencilValue
+
+		pencilPtr1.Cost = 4
+		fmt.Println(pencilValue)
+		fmt.Println(pencilPtr1)
+		fmt.Println(pencilPtr2)
+	*/
+}
+
+/*
+	Write the following functions
+		Format(?)
+			return the formatted string (below) for the given product
+				Id = <val>, Name = "<val>", Cost = <val>
+		ApplyDiscount(?)
+			update the given product cost after applying the given discount percentage
+*/
