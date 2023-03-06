1148e8bfff42db919227be7febc8b10535f8bfc4
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-06 12:38:12 +0530

    Assignment - 05 - solution

--- 11-methods/assignment-05.go
+++ 11-methods/assignment-05.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 /*
@@ -53,8 +54,77 @@ func (product Product) Format() string {
 	return fmt.Sprintf("Id = %d, Name = %q, Cost = %.2f, Units = %d, Category = %q", product.Id, product.Name, product.Cost, product.Units, product.Category)
 }
 
+type Products []Product
+
+func (products Products) Format() string {
+	var sb strings.Builder
+	for _, product := range products {
+		sb.WriteString(fmt.Sprintf("%s\n", product.Format()))
+	}
+	return sb.String()
+}
+
+func (products Products) IndexOf(p Product) int {
+	for idx, product := range products {
+		if product == p {
+			return idx
+		}
+	}
+	return -1
+}
+
+/*
+func (products Products) FilterCostlyProducts() Products {
+	result := Products{}
+	for _, product := range products {
+		if product.Cost > 1000 {
+			result = append(result, product)
+		}
+	}
+	return result
+}
+
+func (products Products) FilterStationaryProducts() Products {
+	result := Products{}
+	for _, product := range products {
+		if product.Category == "Stationary" {
+			result = append(result, product)
+		}
+	}
+	return result
+}
+*/
+
+func (products Products) Filter(predicate func(Product) bool) Products {
+	result := Products{}
+	for _, product := range products {
+		if predicate(product) {
+			result = append(result, product)
+		}
+	}
+	return result
+}
+
+func (products Products) Any(predicate func(Product) bool) bool {
+	for _, product := range products {
+		if predicate(product) {
+			return true
+		}
+	}
+	return false
+}
+
+func (products Products) All(predicate func(Product) bool) bool {
+	for _, product := range products {
+		if !predicate(product) {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
-	products := []Product{
+	products := Products{
 		Product{105, "Pen", 5, 50, "Stationary"},
 		Product{107, "Pencil", 2, 100, "Stationary"},
 		Product{103, "Marker", 50, 20, "Utencil"},
@@ -63,4 +133,29 @@ func main() {
 		Product{104, "Scribble Pad", 20, 20, "Stationary"},
 		Product{109, "Golden Pen", 2000, 20, "Stationary"},
 	}
+	stove := Product{102, "Stove", 5000, 5, "Utencil"}
+	fmt.Println("Index of stove :", products.IndexOf(stove))
+
+	fmt.Println("Initial List")
+	fmt.Println(products.Format())
+
+	fmt.Println("Costly Products")
+	costlyProductPredicate := func(p Product) bool {
+		return p.Cost > 1000
+	}
+	// costlyProducts := products.FilterCostlyProducts()
+	costlyProducts := products.Filter(costlyProductPredicate)
+	fmt.Println(costlyProducts.Format())
+
+	fmt.Println("Stationary Products")
+	// stationaryProducts := products.FilterStationaryProducts()
+	stationaryProductPredicate := func(p Product) bool {
+		return p.Category == "Stationary"
+	}
+	stationaryProducts := products.Filter(stationaryProductPredicate)
+	fmt.Println(stationaryProducts.Format())
+
+	fmt.Println("Are all the products costly products ? :", products.All(costlyProductPredicate))
+	fmt.Println("Are there any costly products ? :", products.Any(costlyProductPredicate))
+
 }
