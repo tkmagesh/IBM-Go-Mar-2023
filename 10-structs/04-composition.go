4d063cc8cd148f3326b1de43ed9ca12585f5a8ce
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-06 10:53:54 +0530

    struct composition

---
+++ 10-structs/04-composition.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type Product struct {
+	Id   int
+	Name string
+	Cost float32
+}
+
+type Dummy struct {
+	Name string
+}
+
+type PerishableProduct struct {
+	Product
+	// Dummy
+	// Name   string // overriding Product.Name
+	Expiry string
+}
+
+func NewPerishableProduct(id int, name string, cost float32, expiry string) PerishableProduct {
+	return PerishableProduct{
+		Product: Product{
+			Id:   id,
+			Name: name,
+			Cost: cost,
+		},
+		Expiry: expiry,
+	}
+}
+
+func main() {
+	// grapes := PerishableProduct{Product{100, "Grapes", 50}, "2 Days"}
+	/*
+		grapes := PerishableProduct{
+			Product: Product{
+				Id:   100,
+				Name: "Grapes",
+				Cost: 50,
+			},
+			Expiry: "2 Days",
+			// Name:   "Arabian Grapes",
+		}
+	*/
+	grapes := NewPerishableProduct(100, "Grapes", 50, "2 Days")
+	fmt.Println(grapes)
+	fmt.Println(grapes.Product.Id, grapes.Product.Name, grapes.Product.Cost, grapes.Expiry)
+	fmt.Println(grapes.Id, grapes.Name, grapes.Cost, grapes.Expiry)
+}
