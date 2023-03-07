19e6619cca22fc8836d43d8fe66edebfaabdba76
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-07 15:16:47 +0530

    18-a-assignment - solution

--- 14-concurrency/18-a-assignment.go
+++ 14-concurrency/18-a-assignment.go
@@ -9,26 +9,66 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 func main() {
 	ch := make(chan int)
-	go genPrimes(3, 100, ch)
-	for primeNo := range ch {
-		fmt.Println(primeNo)
-	}
+	wg := &sync.WaitGroup{}
+
+	wg.Add(1)
+	go genPrimes(3, 100, ch, wg)
+
+	wg.Add(1)
+	go genPrimes(101, 200, ch, wg)
+
+	wg.Add(1)
+	go genPrimes(201, 300, ch, wg)
+
+	//using a waitgroup
+	/*
+		wg2 := &sync.WaitGroup{}
+		wg2.Add(1)
+		go func() {
+			for primeNo := range ch {
+				fmt.Println(primeNo)
+			}
+			fmt.Println("All prime numbers received!")
+			wg2.Done()
+		}()
+
+		wg.Wait()
+		close(ch)
+		wg2.Wait()
+		fmt.Println("Done!")
+	*/
+
+	//using a channel
+	done := make(chan struct{})
+	go func() {
+		for primeNo := range ch {
+			fmt.Println(primeNo)
+		}
+		fmt.Println("All prime numbers received!")
+		// done <- struct{}{}
+		close(done)
+	}()
+
+	wg.Wait()
+	close(ch)
+	<-done
 	fmt.Println("Done!")
 }
 
-func genPrimes(start, end int, ch chan int) {
+func genPrimes(start, end int, ch chan int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for no := start; no <= end; no++ {
 		if isPrime(no) {
 			ch <- no
 			time.Sleep(500 * time.Millisecond)
 		}
 	}
-	close(ch)
 }
 
 func isPrime(no int) bool {
