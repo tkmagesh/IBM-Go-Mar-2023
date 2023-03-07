89f580ee0f04719c47d855e42fef9b96fcca6ed2
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-07 12:42:14 +0530

    more on channels

--- 14-concurrency/10-demo.go
+++ 14-concurrency/10-demo.go
@@ -14,13 +14,13 @@ func main() {
 	ch := make(chan int)
 	wg.Add(1)
 	go add(100, 200, wg, ch)
-	wg.Wait()
 	result := <-ch
+	wg.Wait()
 	fmt.Println("result :", result)
 }
 
 func add(x, y int, wg *sync.WaitGroup, ch chan int) {
-	defer wg.Done()
 	result := x + y
 	ch <- result
+	wg.Done()
 }
